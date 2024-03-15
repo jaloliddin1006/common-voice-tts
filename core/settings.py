cb5bdec4bf616e109ce7a450c196efff28b1d0ee
Jaloliddin Mamatmusayev <jmamatmusayev@gmail.com>
2024-03-15 19:13:36 +0500

    update static

--- core/settings.py
+++ core/settings.py
@@ -118,6 +118,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'#Location of static files
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')#Location of static files
 
 
 MEDIA_URL = '/media/'
